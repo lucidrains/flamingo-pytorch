fix masking issue, thanks to @dhansmair

flamingo_pytorch/flamingo_pytorch.py
@@ -176,7 +176,7 @@ class MaskedCrossAttention(nn.Module):
             # any text without a preceding media needs to have attention zeroed out
             text_without_media_mask = text_time == 0
             text_without_media_mask = rearrange(text_without_media_mask, 'b i -> b 1 i 1')
-            attn.masked_fill(text_without_media_mask, 0.)
+            attn = attn.masked_fill(text_without_media_mask, 0.)
 
         out = einsum('... i j, ... j d -> ... i d', attn, v)
         out = rearrange(out, 'b h n d -> b n (h d)')